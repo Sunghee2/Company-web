d0def7655614427bcef604d6a82ed0856a72e33e
Sunghee2 <630sunghee@naver.com>
2018-05-21 22:47:40 +0900

    add project search

--- routes/projects.js
+++ routes/projects.js
@@ -6,7 +6,46 @@ const conn = mysql.createConnection(require('../config/dbconfig.js'));
 conn.connect()
 
 router.get('/', (req, res, next) => {
-    res.render('projects/index');
+    const start_date = req.query.start_date;
+    const end_date = req.query.end_date;
+
+    if (start_date && end_date) {
+        conn.query('SELECT project_id, project_name, start_date, end_date, name from (SELECT * FROM projects p, employees e WHERE p.pm_number=e.employee_number) t WHERE end_date <= ? AND start_date >= ?', [end_date, start_date],(err, rows) => {
+            if (err) {
+              req.flash('danger', err);
+              return res.redirect('back');  
+            }  
+            const projects = JSON.parse(JSON.stringify(rows));
+            res.render('projects/index', {projects: projects, start_date: start_date, end_date: end_date});
+        });
+    } else if (start_date) {
+        conn.query('SELECT project_id, project_name, start_date, end_date, name from (SELECT * FROM projects p, employees e WHERE p.pm_number=e.employee_number) t WHERE (end_date >= ? OR end_date is NULL) AND start_date >= ?', [start_date, start_date],(err, rows) => {
+            if (err) {
+              req.flash('danger', err);
+              return res.redirect('back');  
+            }  
+            const projects = JSON.parse(JSON.stringify(rows));
+            res.render('projects/index', {projects: projects, start_date: start_date});
+        });
+    } else if (end_date) {
+        conn.query('SELECT project_id, project_name, start_date, end_date, name from (SELECT * FROM projects p, employees e WHERE p.pm_number=e.employee_number) t WHERE end_date <= ? AND start_date <= ?', [end_date, end_date],(err, rows) => {
+            if (err) {
+              req.flash('danger', err);
+              return res.redirect('back');  
+            }  
+            const projects = JSON.parse(JSON.stringify(rows));
+            res.render('projects/index', {projects: projects, end_date: end_date});
+        });
+    } else {
+        conn.query('SELECT project_id, project_name, start_date, end_date, name from (SELECT * FROM projects p, employees e WHERE p.pm_number=e.employee_number) t WHERE end_date >= CURRENT_TIMESTAMP OR end_date is NULL', (err, rows) => {
+            if (err) {
+              req.flash('danger', err);
+              return res.redirect('back');  
+            }  
+            const projects = JSON.parse(JSON.stringify(rows));
+            res.render('projects/index', {projects: projects});
+        });
+    }
 });
 
 module.exports = router;
