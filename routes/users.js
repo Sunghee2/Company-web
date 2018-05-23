1cf4ac09ab8b271f9115cb61ba648d0b26a3fba7
Sunghee2 <630sunghee@naver.com>
2018-05-24 01:16:16 +0900

    edit signup

--- routes/users.js
+++ routes/users.js
@@ -16,24 +16,16 @@ function needAuth(req, res, next) {
 }
 
 function validateForm(form, type, options) {
-  var name = form.name || "";
   var rrn = form.rrn || "";
-  var gender = form.gender || "";
-  var department = form.department || "";
-  var final_education = form.final_education || "";
-  var phone_number = form.phone_number || "";
   
-  name = name.trim();
   rrn = rrn.trim();
-  final_education = final_education.trim();
-  phone_number = phone_number.trim();
-  
+
   if (type === 'signup') {
     var id = form.id || "";
     var employee_number = form.employee_number || "";
     id = id.trim();
     employee_number = employee_number.trim();
-
+    
     if (!id) {
       return 'ID is required.';
     }
@@ -43,40 +35,50 @@ function validateForm(form, type, options) {
     if (!form.password && options.needPassword) {
       return 'Password is required.';
     }
-  
+    
     if (form.password !== form.password_confirmed) {
       return 'Passsword do not match.';
     }
-  
+    
     if (form.password.length < 6) {
       return 'Password must be at least 6 characters.';
     }
   }
+  if (type == 'edit') {
+    var name = form.name || "";
+    var gender = form.gender || "";
+    var department = form.department || "";
+    var final_education = form.final_education || "";
+    var phone_number = form.phone_number || "";
+    
+    name = name.trim();
+    final_education = final_education.trim();
+    phone_number = phone_number.trim();
+    
+    if (!name) {
+      return 'Name is required.';
+    }
+    
+    if (!rrn) {
+      return 'RRN is required.';
+    }
+    
+    if (!department) {
+      return 'Department is required.';
+    }
 
-  if (!name) {
-    return 'Name is required.';
-  }
-  
-  if (!rrn) {
-    return 'RRN is required.';
-  }
+    if (!gender) {
+      return 'Gender is required.';
+    }
 
-  if (!department) {
-    return 'Department is required.';
-  }
+    if (!final_education) {
+      return 'Final education is required.';
+    }
 
-  if (!gender) {
-    return 'Gender is required.';
+    if (!phone_number) {
+      return 'Phone number is required.';
+    }
   }
-
-  if (!final_education) {
-    return 'Final education is required.';
-  }
-
-  if (!phone_number) {
-    return 'Phone number is required.';
-  }
-
   return null;
 }
 
@@ -115,52 +117,57 @@ router.route('/')
     var id = req.body.id;
     var password = req.body.password;
     var employee_number = req.body.employee_number;
-    var name = req.body.name;
     var rrn = req.body.rrn;
-    var department = req.body.department;
-    var gender = req.body.gender;
-    var final_education = req.body.final_education;
-    var email = req.body.email || "";
-    var phone_number = req.body.phone_number;
-
     conn.query('SELECT * FROM accounts WHERE employee_number=?', [employee_number], function(err, rows) {
       if (err) {
         req.flash('danger', err);
         return res.redirect('back');
       }
-      if (!rows.length) {
-        bcrypt.hash(password, 10, function(err, hash) {
-            var account = [
-              // employee_number, id, hash
-              id, hash, employee_number
-            ];
-            var employee = [
-              employee_number, name, rrn, department,
-              gender, final_education, email, phone_number
-            ]
-            conn.query('INSERT INTO employees values (?,?,?,?,?,?,?,?)', employee);
-            conn.query('INSERT INTO accounts values (?,?,?)', account);
-            /*
-            conn.query('INSERT INTO employees values (?,?,?,?,?,?,?,?)', employee, function(err, rows2) {
+      if (rows.length >= 0) {
+        conn.query('SELECT * FROM accounts WHERE id=?', [id], (err, rows3) => {
+          if (err) {
+            req.flash('danger', err);
+            return res.redirect('back');           
+          }
+          if (rows3.length <= 0) {
+            conn.query('SELECT * FROM employees WHERE employee_number=?', [employee_number], (err, rows2) => {
               if (err) {
                 req.flash('danger', err);
                 return res.redirect('back');
               }
-            })
-            conn.query('INSERT INTO accounts values (?,?,?)', account, function(err, rows3) {
-              if (err) {
-                req.flash('danger', err);
+              if (rows2.length) {
+                if (rows2[0].RRN != rrn) {
+                  req.flash('danger', 'RRN이 올바르지 않습니다');
+                  return res.redirect('back');
+                } else {
+                  bcrypt.hash(password, 10, function(err, hash) {
+                    var account = [
+                      employee_number, id, hash
+                    ];
+                    conn.query('INSERT INTO accounts values (?,?,?)', account, (err, rows4) => {
+                      if (err) {
+                        req.flash('danger', err);
+                        return res.redirect('back');
+                      }
+                      req.flash('success', 'Registered successfully. Please sign in.');
+                      return res.redirect('/signin');
+                    });
+                  })
+                }
+              } else {
+                req.flash('danger', '존재하지 않는 사번입니다.');
                 return res.redirect('back');
               }
             })
-            */
-          })
-        } else {
+          } else {
+            req.flash('danger', '이미 존재하는 아이디입니다. 다른 아이디를 입력해주세요');
+            return res.redirect('back');
+          }
+        })
+      } else {
           req.flash('danger', '이미 가입된 사번입니다.');
           return res.redirect('back');
         }
-        req.flash('success', 'Registered successfully. Please sign in.');
-        res.redirect('/signin');
       })
   }));
 
